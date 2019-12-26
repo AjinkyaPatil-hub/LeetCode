f1f0d74168347bf8366e6739790599ff8f03e4af
AjinkyaPatil-hub <52791315+AjinkyaPatil-hub@users.noreply.github.com>
2019-12-27 03:21:34 +0530

    Update Main.java

--- src/com/LeetCode/ArrayList/Main.java
+++ src/com/LeetCode/ArrayList/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 		public static void main(String[] args) {
 			MyArrayList al = new MyArrayList();
+			//merge failed; fix conflicts and then commit the result.
 			al.add(8);
 			al.add(7);
 			al.add(99);
